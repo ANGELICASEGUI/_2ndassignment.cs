7cd0dd8335f8e32e6f1e742ffb5f009a3535c69b
ANGELICASEGUI <165160266+ANGELICASEGUI@users.noreply.github.com>
2024-03-31 09:34:40 +0800

    Add files via upload

---
+++ _2ndassignment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace _2ndassignment
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int Num = 1; Num <=100; Num++)
+            {
+                
+                if (Num % 3 == 0)
+                {
+                    Console.WriteLine("HELLO");
+                }
+                else if (Num % 5 == 0)
+                {
+                    Console.WriteLine("GOODBYE");
+                }
+                else if (Num % 15 == 0)
+                {
+                    Console.WriteLine("HELLO GOODBYE");
+                }
+                else
+                {
+                    Console.WriteLine(Num);
+                }
+
+                
+            
+                   
+
+
+
+            }
+
+
+        }
+    }
+}
